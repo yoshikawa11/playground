cc2035caf3e7ba31509267fb714d079e156645af
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-10-22 07:53:59 +0900

    hello finished

---
+++ learn-go-with-tests/hello/hello.go
@@ -0,0 +1,27 @@
+package main
+
+const spanish = "Spanish"
+const french = "French"
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjou, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
