e514c79a221491356a75f89291ad6696c3765435
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-10-29 12:40:25 +0900

    q28: finished

---
+++ leetcode-go/answers/find-the-index-of-the-first-occurrence-in-a-string.go
@@ -0,0 +1,17 @@
+package main
+
+func strStr(haystack string, needle string) int {
+	if len(needle) == 0 {
+		return 0
+	}
+	if len(haystack) < len(needle) {
+		return -1
+	}
+
+	for i := 0; i <= len(haystack)-len(needle); i++ {
+		if haystack[i:i+len(needle)] == needle {
+			return i
+		}
+	}
+	return -1
+}
