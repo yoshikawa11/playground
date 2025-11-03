cf7045895658bbb0a102c0b8a9f678d7e551ba05
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-11-03 13:36:10 +0900

    q35: finished

---
+++ leetcode-go/answers/search-iinsert-position.go
@@ -0,0 +1,20 @@
+package main
+
+func searchInsert(nums []int, target int) int {
+	left := 0
+	right := len(nums) - 1
+
+	for left <= right {
+		mid := (left + right) / 2
+		if nums[mid] == target {
+			return mid
+		}
+		if nums[mid] < target {
+			left = mid + 1
+		} else {
+			right = mid - 1
+		}
+	}
+
+	return left
+}
