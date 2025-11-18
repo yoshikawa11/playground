7b2f6a7b9cf0e8f1ac72d5c8c05bfa95147f08fd
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-11-18 22:41:16 +0900

    [wip]http-server

--- learn-go-with-tests/http-server/server.go
+++ learn-go-with-tests/http-server/server.go
@@ -3,8 +3,18 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strings"
 )
 
-func PlayerServer(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "20")
+type PlayerStore interface {
+	GetPlayerScore(name string) int
+}
+
+type PlayerServer struct {
+	store PlayerStore
+}
+
+func (p *PlayerServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	player := strings.TrimPrefix(r.URL.Path, "/players/")
+	fmt.Fprint(w, p.store.GetPlayerScore(player))
 }
