bac6028c39288a48bf71f9738c2fd20ce8aaac1a
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-11-21 22:30:30 +0900

    http-server: finished

--- learn-go-with-tests/http-server/main.go
+++ learn-go-with-tests/http-server/main.go
@@ -5,14 +5,8 @@ import (
 	"net/http"
 )
 
-type InMemoryPlayerStore struct{}
-
-func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	return 123
-}
-
 func main() {
-	server := &PlayerServer{&InMemoryPlayerStore{}}
+	server := &PlayerServer{NewInMemoryPlayerStore()}
 
 	log.Fatal(http.ListenAndServe(":5000", server))
 }
