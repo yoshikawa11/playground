4fe747950c378cb1a2eff1a91975c5922eeee821
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-12-09 20:37:44 +0900

    [wip]time

---
+++ learn-go-with-tests/time/CLI.go
@@ -0,0 +1,41 @@
+package poker
+
+import (
+	"bufio"
+	"io"
+	"strings"
+	"time"
+)
+
+type CLI struct {
+	playerStore PlayerStore
+	in          *bufio.Scanner
+	alerter     BlindAlerter
+}
+
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+func NewCLI(store PlayerStore, in io.Reader, alerter BlindAlerter) *CLI {
+	return &CLI{
+		playerStore: store,
+		in:          bufio.NewScanner(in),
+		alerter:     alerter,
+	}
+}
+
+func (cli *CLI) PlayPoker() {
+	cli.alerter.ScheduleAlertAt(5*time.Second, 100)
+	userInput := cli.readLine()
+	cli.playerStore.RecordWin(extractWinner(userInput))
+}
+
+func extractWinner(userInput string) string {
+	return strings.Replace(userInput, " wins", "", 1)
+}
+
+func (cli *CLI) readLine() string {
+	cli.in.Scan()
+	return cli.in.Text()
+}
---
+++ learn-go-with-tests/time/CLI_test.go
@@ -0,0 +1,65 @@
+package poker_test
+
+import (
+	poker "learn-go-with-tests/time"
+	"strings"
+	"testing"
+	"time"
+)
+
+type BlindAlerter interface {
+	ScheduleAlertAt(duration time.Duration, amount int)
+}
+
+type SpyBlindAlerter struct {
+	alerts []struct {
+		scheduledAt time.Duration
+		amount      int
+	}
+}
+
+func (s *SpyBlindAlerter) ScheduleAlertAt(duration time.Duration, amount int) {
+	s.alerts = append(s.alerts, struct {
+		scheduledAt time.Duration
+		amount      int
+	}{duration, amount})
+}
+
+func TestCLI(t *testing.T) {
+
+	t.Run("record chris win from user input", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		var dummySpyAlerter = &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Chris")
+	})
+
+	t.Run("record cleo win from user input", func(t *testing.T) {
+		in := strings.NewReader("Cleo wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		var dummySpyAlerter = &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, dummySpyAlerter)
+		cli.PlayPoker()
+
+		poker.AssertPlayerWin(t, playerStore, "Cleo")
+	})
+
+	t.Run("it schedules printing of blind values", func(t *testing.T) {
+		in := strings.NewReader("Chris wins\n")
+		playerStore := &poker.StubPlayerStore{}
+		blindAlerter := &SpyBlindAlerter{}
+
+		cli := poker.NewCLI(playerStore, in, blindAlerter)
+		cli.PlayPoker()
+
+		if len(blindAlerter.alerts) != 1 {
+			t.Fatal("expected a blind alert to be scheduled")
+		}
+	})
+
+}
