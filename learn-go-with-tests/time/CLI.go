07a9519f7880cee86baacd5c1c7f955bb0f948b6
yoshikawa masashi <gunterriote@yahoo.co.jp>
2025-12-11 22:07:22 +0900

    [wip]time

--- learn-go-with-tests/time/CLI.go
+++ learn-go-with-tests/time/CLI.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"io"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -28,17 +29,23 @@ const PlayerPrompt = "Please enter the number of players: "
 
 func (cli *CLI) PlayPoker() {
 	fmt.Fprint(cli.out, PlayerPrompt)
-	cli.scheduleBlindAlerts()
+
+	numberOfPlayers, _ := strconv.Atoi(cli.readLine())
+
+	cli.scheduleBlindAlerts(numberOfPlayers)
+
 	userInput := cli.readLine()
 	cli.playerStore.RecordWin(extractWinner(userInput))
 }
 
-func (cli *CLI) scheduleBlindAlerts() {
+func (cli *CLI) scheduleBlindAlerts(numberOfPlayers int) {
+	blindIncrement := time.Duration(5+numberOfPlayers) * time.Minute
+
 	blinds := []int{100, 200, 300, 400, 500, 600, 800, 1000, 2000, 4000, 8000}
 	blindTime := 0 * time.Second
 	for _, blind := range blinds {
 		cli.alerter.ScheduleAlertAt(blindTime, blind)
-		blindTime = blindTime + 10*time.Minute
+		blindTime = blindTime + blindIncrement
 	}
 }
 
